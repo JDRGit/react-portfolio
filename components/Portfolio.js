fd27da63f0c6fa0d8b7a232acc7b6c2d3f594fc1
JDRGit <jaimerodriguez85@gmail.com>
2023-05-11 20:54:50 -0400

    fixed alignment of cards, updated resume

--- components/Portfolio.js
+++ components/Portfolio.js
@@ -6,7 +6,6 @@ import web1 from "../public/web1.png";
 import web2 from "../public/web2.png";
 import web3 from "../public/web3.png";
 
-
 const Portfolio = () => {
   const [selectedId, setSelectedId] = useState(null);
 
@@ -43,63 +42,65 @@ const Portfolio = () => {
 
   return (
     <section>
-      <h2 className="text-3xl font-semibold text-center mb-8  dark:text-gray-200">Portfolio</h2>
+      <h2 className="text-3xl font-semibold text-center mb-8  dark:text-gray-200">
+        Portfolio
+      </h2>
       <MotionConfig>
-        <div className="flex flex-col gap-10 px-4 lg:px-0 py-10 lg:flex-row lg:flex-wrap">
-        {projects.map((project) => (
-  <motion.div
-    key={project.id}
-    className="flex flex-col gap-6 bg-white rounded-lg shadow-lg p-4 cursor-pointer transform transition-transform duration-500 hover:scale-105"
-    layout
-    onClick={() => handleClick(project.id)}
-  >
-    <motion.div
-      layout
-      className={`flex flex-col items-center ${
-        selectedId === project.id ? "space-y-4" : ""
-      }`}
-    >
-      <div className="relative w-full h-60">
-        <Image
-          src={project.image}
-          layout="fill"
-          className="rounded-lg object-cover"
-          alt={project.name}
-        />
-      </div>
-      <motion.h3 layout className="text-xl font-medium pt-2">
-        {project.name}
-      </motion.h3>
-    </motion.div>
-    <AnimatePresence>
-      {selectedId === project.id && (
-        <>
-          <motion.p layout className="text-gray-700">
-            {project.description}
-          </motion.p>
-          <motion.div
-            layout
-            className="flex items-center justify-between"
-          >
-            <a
-              href={project.github}
-              className="flex items-center space-x-2 text-blue-500 hover:text-blue-600"
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 px-4 lg:px-0 py-10">
+          {projects.map((project) => (
+            <motion.div
+              key={project.id}
+              className="flex flex-col gap-6 bg-white rounded-lg shadow-lg p-4 cursor-pointer transform transition-transform duration-500 hover:scale-105"
+              layout
+              onClick={() => handleClick(project.id)}
             >
-              <FiGithub />
-              <span>Github</span>
-            </a>
-            <a
-              href={project.netlify}
-              className="flex items-center space-x-2 text-green-500 hover:text-green-600"
-            >
-              <FiExternalLink />
-              <span>Netlify</span>
-            </a>
-          </motion.div>
-        </>
-      )}
-    </AnimatePresence>
-  </motion.div>
+              <motion.div
+                layout
+                className={`flex flex-col items-center ${
+                  selectedId === project.id ? "space-y-4" : ""
+                }`}
+              >
+                <div className="relative w-full h-60">
+                  <Image
+                    src={project.image}
+                    layout="fill"
+                    className="rounded-lg object-cover"
+                    alt={project.name}
+                  />
+                </div>
+                <motion.h3 layout className="text-xl font-medium pt-2">
+                  {project.name}
+                </motion.h3>
+              </motion.div>
+              <AnimatePresence>
+                {selectedId === project.id && (
+                  <>
+                    <motion.p layout className="text-gray-700">
+                      {project.description}
+                    </motion.p>
+                    <motion.div
+                      layout
+                      className="flex items-center justify-between"
+                    >
+                      <a
+                        href={project.github}
+                        className="flex items-center space-x-2 text-blue-500 hover:text-blue-600"
+                      >
+                        <FiGithub />
+                        <span>Github</span>
+                      </a>
+                      <a
+                        href={project.netlify}
+                        className="flex items-center space-x-2 text-green-500 hover:text-green-600"
+                      >
+                        <FiExternalLink />
+                        <span>Netlify</span>
+                      </a>
+                    </motion.div>
+                  </>
+                )}
+              </AnimatePresence>
+            </motion.div>
           ))}
         </div>
       </MotionConfig>
